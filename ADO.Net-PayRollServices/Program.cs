dda9e2e7f3e94f6a782d6296e17cf23af925855a
PriyankaRumal <priyankarumal182000@gmail.com>
2022-12-12 18:44:29 +0530

    Solutuin added

---
+++ ADO.Net-PayRollServices/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ADO.Net_PayRollServices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcom To SQL Program");
+            EmployeeRepository.GetAllEmployee();
+            Console.ReadLine();
+        }
+    }
+}
